98258496a8612c6f1d1cfc3e32691001618653a1
Jessica <fr_jess@icloud.com>
2023-01-27 10:42:19 +0100

    [fix] import router

---
+++ resources/js/pages/Error404.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'Error404',
+}
+
+</script>
+
+<template>
+    <h1>Error 404</h1>
+
+</template>
+
+<style>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -3,6 +3,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 import Contacts from './pages/Contacts.vue';
+import Error404 from './pages/Error404.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,10 @@ const router = createRouter({
             path: '/contacts',
             name:'contacts',
             component: Contacts
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            component: Error404
         },
     ]
 })
