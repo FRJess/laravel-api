a52f9647dad1b5edcdd2b56ded0cd345e4495068
Jessica <fr_jess@icloud.com>
2023-01-27 15:48:30 +0100

    [add] header content

--- resources/js/partials/Header.vue
+++ resources/js/partials/Header.vue
@@ -6,11 +6,42 @@ export default {
 </script>
 
 <template>
-    <header>
-        header
-    </header>
+    <div class="header text-center">
+        <nav class="navbar navbar-expand text-center navbar-dark bg-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav mb-2">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" :to="{ name: 'home'}">home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" :to="{ name: 'about'}">about</router-link>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <router-link class="nav-link" :to="{ name: 'contacts'}">contacts</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use '../../scss/appVue.scss';
 
+nav{
+    // padding: 30px 0;
+    ul{
+        // list-style: none;
+        li{
+            // display: inline-block;
+            // padding: 10px 20px;
+            a{
+                color: black;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+        }
+    }
+}
 </style>
